202f539b5a0b8b1f5a7530ba0eda3e6fe7ac00e0
Alamin Magoti <alamin.magoti@thejitu.com>
2023-12-18 15:18:51 +0300

    DTO s

--- Repository/PokemonRepository.cs
+++ Repository/PokemonRepository.cs
@@ -1,4 +1,5 @@
 ﻿using PokemonReviewApp.Data;
+using PokemonReviewApp.Dto;
 using PokemonReviewApp.Interfaces;
 using PokemonReviewApp.Models;
 
@@ -8,9 +9,39 @@ namespace PokemonReviewApp.Repository
     {
         private readonly DataContext _context;
 
-        public PokemonRepository(DataContext context)
+        public PokemonRepository(DataContext context) 
         {
             _context = context;
+        }
+
+        public bool CreatePokemon(int ownerId, int categoryId, Pokemon pokemon)
+        {
+            throw new NotImplementedException();
+        }
+
+        public bool DeletePokemon(Pokemon pokemon)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Pokemon GetPokemon(int id)
+        {
+            return _context.Pokemon.Where(p => p.Id == id).FirstOrDefault();
+        }
+
+        public Pokemon GetPokemon(string name)
+        {
+            return _context.Pokemon.Where(p => p.Name == name).FirstOrDefault();
+        }
+
+        public decimal GetPokemonRating(int pokeId)
+        {
+            var review = _context.Reviews.Where(p => p.Pokemon.Id == pokeId);
+
+            if (review.Count() <= 0)
+                return 0;
+
+            return ((decimal)review.Sum(r => r.Rating) / review.Count());
         }
 
         public ICollection<Pokemon> GetPokemons()
@@ -18,5 +49,24 @@ namespace PokemonReviewApp.Repository
             return _context.Pokemon.OrderBy(p => p.Id).ToList();
         }
 
+        public Pokemon GetPokemonTrimToUpper(PokemonDto pokemonCreate)
+        {
+            throw new NotImplementedException();
+        }
+
+        public bool PokemonExists(int pokeId)
+        {
+            return _context.Pokemon.Any(p => p.Id == pokeId);
+        }
+
+        public bool Save()
+        {
+            throw new NotImplementedException();
+        }
+
+        public bool UpdatePokemon(int ownerId, int categoryId, Pokemon pokemon)
+        {
+            throw new NotImplementedException();
+        }
     }
 }
